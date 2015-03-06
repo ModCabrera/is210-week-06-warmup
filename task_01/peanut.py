ff058f1ec0e06b7228732aedff55ee45da503ebf
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-06 04:48:59 +0000

    first push

---
+++ task_01/peanut.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Peanut Docstring"""
+
+
+def butter():
+    print 'I am truthy'
+    return True
+
+
+def oil():
+    print 'I am Falsy'
+    return False
+
+
+OIL = oil()
+
+BUTTER = butter()
